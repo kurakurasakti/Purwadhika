360e4fd4c481bfb65eebb9f87634c0c3292821c8
kurakurasakti <roysaputra_9@hotmail.com>
2019-02-11 10:23:58 +0700

    duplikat function

--- day14/duplikatFunc.js
+++ day14/duplikatFunc.js
@@ -1,20 +1,22 @@
-var a = [4,3,5,7,1,2]
+//#region mapp
+// var a = [4,3,5,7,1,2]
 
-var anon = function(val){
-    return val * 2 
-}
+// var anon = function(val){
+//     return val * 2 
+// }
 
-function filterMaping(arr, fn){
-    let newArr = []
-    //var arr = [...arr]
-    for (let i = 0; i < arr.length; i++) {
-     newArr.push(fn(arr[i]))
+// function filterMaping(arr, fn){
+//     let newArr = []
+//     //var arr = [...arr]
+//     for (let i = 0; i < arr.length; i++) {
+//      newArr.push(fn(arr[i]))
         
-    }
-    return newArr
-}
-
-console.log(filterMaping(a, anon));
+//     }
+//     return newArr
+// }
+//#endregion
+//#region filter
+// console.log(filterMaping(a, anon));
 // function filterDupe(arr,fn){
 //     var newArr = []
 //     for(var i=0; i<a.length;i++){
@@ -26,5 +28,25 @@ console.log(filterMaping(a, anon));
 // }
 
 // console.log(filterDupe(a,anon));
+//#endregion
+//#region sort
+var a = [1,4,5,6,7,8]
 
+var sortDupe = (arr, fn) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            if (fn(arr[i], arr[j]<0)) {
+                var temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp;
+            }
+            
+        }
+        
+    }
+    return arr
+}
 
+sortDupe(a,function(a,b){return a-b})
+
+//#endregion
